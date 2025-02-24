381b3dbf7d290152d64f750be9989c9917f0aec1
Sudeep MP <sudipmelepunathilmp@gmail.com>
2025-02-24 18:14:45 +0000

    Enhance front_app display handling for multi-monitor setups
    - Added support for multi-display front app tracking
    - Implemented dynamic visibility based on focused monitor
    - Updated front_app.sh and front_app plugin to handle display-specific app information
    - Added monitor focus event listener
    - Improved app icon and label updates for specific displays

--- items/front_app.sh
+++ items/front_app.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 
-sketchybar --add item front_app left \
-  --set front_app background.color=$ACCENT_COLOR \
-  icon.color=$BAR_COLOR \
-  icon.font="sketchybar-app-font:Regular:16.0" \
-  label.color=$BAR_COLOR \
-  script="$PLUGIN_DIR/front_app.sh" \
-  --subscribe front_app front_app_switched
+# Add event for monitor focus changes
+sketchybar --add event monitor_focus
+
+DISPLAY_COUNT=$(sketchybar --query displays | jq 'length')
+
+if [ "$DISPLAY_COUNT" -eq 1 ]; then
+  MAIN_DISPLAY=$(sketchybar --query displays | jq -r '.[0].DirectDisplayID')
+  sketchybar --add item front_app_main left \
+    --set front_app_main background.color=$ACCENT_COLOR \
+      icon.color=$BAR_COLOR \
+      icon.font="sketchybar-app-font:Regular:16.0" \
+      label.color=$BAR_COLOR \
+      associated_display=$MAIN_DISPLAY \
+      script="$PLUGIN_DIR/front_app.sh" \
+      --subscribe front_app_main front_app_switched monitor_focus
+else
+  MAIN_DISPLAY=$(sketchybar --query displays | jq -r '.[1].DirectDisplayID')
+  SECONDARY_DISPLAY=$(sketchybar --query displays | jq -r '.[0].DirectDisplayID')
+  sketchybar --add item front_app_main left \
+    --set front_app_main background.color=$ACCENT_COLOR \
+      icon.color=$BAR_COLOR \
+      icon.font="sketchybar-app-font:Regular:16.0" \
+      label.color=$BAR_COLOR \
+      associated_display=$MAIN_DISPLAY \
+      script="$PLUGIN_DIR/front_app.sh" \
+      --subscribe front_app_main front_app_switched monitor_focus
+
+  sketchybar --add item front_app_secondary left \
+    --set front_app_secondary background.color=$ACCENT_COLOR \
+      icon.color=$BAR_COLOR \
+      icon.font="sketchybar-app-font:Regular:16.0" \
+      label.color=$BAR_COLOR \
+      associated_display=$SECONDARY_DISPLAY \
+      script="$PLUGIN_DIR/front_app.sh" \
+      --subscribe front_app_secondary front_app_switched monitor_focus
+fi
--- plugins/front_app.sh
+++ plugins/front_app.sh
@@ -1,10 +1,66 @@
 #!/bin/sh
 
-# Some events send additional information specific to the event in the $INFO
-# variable. E.g. the front_app_switched event sends the name of the newly
-# focused application in the $INFO variable:
-# https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
+# Function to get clean monitor ID
+get_monitor_id() {
+  echo "$1" | cut -d '|' -f 1 | tr -d ' '
+}
 
-if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set $NAME label="$INFO" icon="$($CONFIG_DIR/plugins/icon_map_fn.sh "$INFO")"
+# Function to update item visibility based on focused monitor
+update_visibility() {
+  local name=$1
+  local item_display=$2
+  local focused_monitor=$3
+  
+  # Clean up monitor IDs for comparison
+  local clean_item_display=$(get_monitor_id "$item_display")
+  local clean_focused_monitor=$(get_monitor_id "$focused_monitor")
+  
+  if [ "$clean_item_display" = "$clean_focused_monitor" ]; then
+    sketchybar --set "$name" drawing=on
+    return 0
+  else
+    sketchybar --set "$name" drawing=off
+    return 1
+  fi
+}
+
+# Get the focused monitor from aerospace
+FOCUSED_MONITOR=$(aerospace list-monitors --focused)
+
+# Get the associated display for the triggered item
+ITEM_DISPLAY=$(sketchybar --query "$NAME" | jq -r '.associated_display // empty')
+
+# If ITEM_DISPLAY is empty, set it based on the item name
+if [ -z "$ITEM_DISPLAY" ]; then
+  if [ "$NAME" = "front_app_main" ]; then
+    ITEM_DISPLAY="2"  # Main display
+  else
+    ITEM_DISPLAY="1"  # Secondary display
+  fi
 fi
+
+case "$SENDER" in
+  "front_app_switched")
+    # Check if $INFO contains a colon
+    if echo "$INFO" | grep -q ':'; then
+      EVENT_DISPLAY=$(echo "$INFO" | cut -d ':' -f 1)
+      APP_NAME=$(echo "$INFO" | cut -d ':' -f 2-)
+    else
+      EVENT_DISPLAY="$ITEM_DISPLAY"
+      APP_NAME="$INFO"
+    fi
+    
+    # Update visibility first
+    if update_visibility "$NAME" "$ITEM_DISPLAY" "$FOCUSED_MONITOR"; then
+      # Only update app info if this is the correct display for the event
+      if [ "$(get_monitor_id "$ITEM_DISPLAY")" = "$(get_monitor_id "$EVENT_DISPLAY")" ]; then
+        ICON="$($CONFIG_DIR/plugins/icon_map_fn.sh "$APP_NAME")"
+        sketchybar --set "$NAME" label="$APP_NAME" icon="$ICON"
+      fi
+    fi
+    ;;
+    
+  "monitor_focus")
+    update_visibility "$NAME" "$ITEM_DISPLAY" "$FOCUSED_MONITOR"
+    ;;
+esac
