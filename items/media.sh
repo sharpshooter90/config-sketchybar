43c5995bc9897646c529238767f2695af6930857
Sudeep MP <sudipmelepunathilmp@gmail.com>
2025-03-03 19:00:46 +0000

    Update time and media display formatting
    - Modified calendar plugin to use 12-hour time format with AM/PM
    - Adjusted media plugin label styling and color configuration
    - Removed redundant clock.sh plugin
    - Fine-tuned font size and padding for media item

--- items/media.sh
+++ items/media.sh
@@ -8,7 +8,12 @@ sketchybar --add item media right \
   icon.color=$ACCENT_COLOR \
   background.drawing=off \
   click_script="aerospace workspace Music" \
-  label.font="BigBlueTerm437 Nerd Font Propo:Regular:12.0" \
+  label.font="BigBlueTerm437 Nerd Font Propo:Regular:11.0" \
+  label.color=$WORKSPACE_LABEL_SECONDARY_COLOR \
+  icon.color=$WORKSPACE_ICON_SECONDARY_COLOR \
+  background.color=$ACTIVE_WORKSPACE_SECONDARY_BG_COLOR \
+  icon.padding_left=12 \
+  label.padding_right=12 \
   script="$PLUGIN_DIR/media.sh" \
   --subscribe media media_change
 
--- plugins/clock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-source "$CONFIG_DIR/colors.sh"
-# The $NAME variable is passed from sketchybar and holds the name of
-# the item invoking this script:
-# https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
-
-sketchybar --set "$NAME" label="$(date '+%d/%m %H:%M')" label.color=$WORKSPACE_LABEL_SECONDARY_COLOR icon.color=$WORKSPACE_ICON_SECONDARY_COLOR background.color=$ACTIVE_WORKSPACE_SECONDARY_BG_COLOR
-
