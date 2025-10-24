7991a4137aca625f7d18cd263576b6a17a2c3882
Sudeep MP <sudipmelepunathilmp@gmail.com>
2025-10-24 20:04:14 +0100

    Update theme to yellow

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Available themes: oxocarbon, teal, gray, purple, red, blue, green, orange, yellow
-THEME="${SKETCHYBAR_THEME:-teal}"
+THEME="${SKETCHYBAR_THEME:-yellow}"
 
 # Common colors
 export WHITE=0xffffffff
